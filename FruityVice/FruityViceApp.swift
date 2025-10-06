4af842c52d84f16a24b42117f0150759f1f75c60
Douglas Jasper <doug.jasper@trios.com>
2025-10-06 08:09:59 -0400

    Initial Commit

---
+++ FruityVice/FruityViceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruityViceApp.swift
+//  FruityVice
+//
+//  Created by Douglas Jasper on 2025-10-06.
+//
+
+import SwiftUI
+
+@main
+struct FruityViceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
